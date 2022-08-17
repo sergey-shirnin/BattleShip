d4cf45038e17c34c16026c82a82b743b2df6513f
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-08-17 16:56:51 +1100

    Stage 2

--- GameException.java
+++ GameException.java
@@ -1,18 +1,19 @@
 package battleship;
 
+
 import java.util.Arrays;
 import java.util.Objects;
 
 
 public class GameException {
 
-    public static String getExceptionCause(int[] shipRowRange,
-                                           int[] shipColRange, int shipSize,
-                                           int[][] shipCoords, int[][][] allShipsCoords) {
+    public static String getPlacementExceptionCause(int[] shipRowRange,
+                                                    int[] shipColRange, int shipSize,
+                                                    int[][] shipCoords, int[][][] allShipsCoords) {
 
         boolean designException = getDesignException(shipRowRange, shipColRange);
         boolean lengthException = getLengthException(shipCoords, shipSize);
-        boolean proximityException = getProximity(shipCoords, allShipsCoords);
+        boolean proximityException = getProximityException(shipCoords, allShipsCoords);
 
         return designException ? "design"
                 : lengthException ? "length"
@@ -28,59 +29,75 @@ public class GameException {
         return shipCoords.length != shipSize;
     }
 
-    public static boolean getProximity(int[][] shipCoords, int[][][] allShipsCoords) {
+    public static boolean getProximityException(int[][] shipCoords, int[][][] allShipsCoords) {
 
         boolean proximity = false;
 
         outerLoop:
         for (int[] coordExisting : Arrays.stream(allShipsCoords)
-                                                        .filter(Objects::nonNull)
-                                                        .flatMap(Arrays::stream)
-                                                        .toArray(int[][]::new)) {
+                                                    .filter(Objects::nonNull)
+                                                    .flatMap(Arrays::stream)
+                                                    .toArray(int[][]::new)) {
             for (int[] coordNew : shipCoords) {
-                int[] test = new int[2];
                 for (int i = 0; i < 2; i++) {
-                    test[i] = coordExisting[i] - coordNew[i];
-                }
-                int testSum = Arrays.stream(test).sum();
-                for (int i = 0; i < test.length; i++) {
-                    if (coordExisting[i] == coordNew[i]) {
-                        if (Math.abs(testSum) == 1) {
-                            proximity = true;
-                            break outerLoop;
-                        }
-                    }
+                    if (proximity) break outerLoop;
+                    proximity = coordExisting[i ^ 0] == coordNew[i ^ 0] &&
+                            Math.abs(coordExisting[i ^ 1] - coordNew[i ^ 1]) == 1;
                 }
             }
         }
         return proximity;
     }
+
+    public static String getShotExceptionCause(int[] shotCoords, int gameSize) {
+
+        boolean shotBeyondException = getBeyondException(shotCoords, gameSize);
+
+        return shotBeyondException ? "beyond"
+                : "SWITCH DEFAULT -> VALID ENTRY";
+    }
+
+    public static boolean getBeyondException(int[] shotCoords, int gameSize) {
+
+        boolean beyond = false;
+
+        for (int xy : shotCoords) {
+            if (beyond) break;
+            beyond = Math.max(0, xy) != Math.min(xy, gameSize - 1);
+        }
+        return beyond;
+    }
 }
+
 
 class ShipDesignException extends Exception {
 
     public ShipDesignException() {
-        super(String.format("%nError! Wrong ship location! Try again:%n"));
+        super(String.format("" +
+                "%nError! Wrong ship location! Try again:%n"));
     }
 }
 
 class ShipLengthException extends Exception {
 
     public ShipLengthException(String shipType) {
-        super(String.format("%nError! Wrong length of the %s! Try again:%n",
-                shipType));
+        super(String.format("" +
+                        "%nError! Wrong length of the %s! Try again:%n", shipType));
     }
 }
 
 class ShipsProximityException extends Exception {
 
     public ShipsProximityException() {
-        super(String.format("%nError! You placed it too close to another one. Try again:%n"));
+        super(String.format("" +
+                "%nError! You placed it too close to another one. Try again:%n"));
     }
 }
 
+class ShotBeyondException extends Exception {
 
-
-
-
-
+    public ShotBeyondException() {
+        super(String.format("" +
+                "%nError! You entered the wrong coordinates! Try again:%n"));
+    }
+}
