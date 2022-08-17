5f9e6fb3e1662203879d8b576af81d3e262e288e
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-08-17 16:56:08 +1100

    Stage 2

--- Main.java
+++ Main.java
@@ -2,24 +2,28 @@ package battleship;
 
 public class Main {
 
-    public static void main(String[] args) throws
-                                            ShipDesignException,
-                                            ShipLengthException,
-                                            ShipsProximityException {
+    public static void main(String[] args) {
         // primary game set up
         final int gameSize = 10;
         final String boardFill = "~";
+        final String shipMark = "O";
+        final String shipHitMark = "X";
+        final String hitMsg = "hit a ship";
+        final String shipMissMark = "M";
+        final String missMsg = "missed";
 
-        Game myGame = new Game(gameSize, boardFill);
+        Game myGame = new Game(gameSize, boardFill,
+                shipMark, shipHitMark, hitMsg, shipMissMark, missMsg);
 
         // set up initial game board
         myGame.setUpBoard();
-        myGame.printBoard();
 
         // place all ships on board
         for (Ships ship : Ships.values()) {
             myGame.placeShip(ship);
-            myGame.printBoard();
         }
+
+        // taking shots
+        myGame.placeShot();
     }
 }
