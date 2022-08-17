c420340ebec1518fd89583f4e24176756dee0112
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-08-17 16:57:57 +1100

    Stage 2

--- Game.java
+++ Game.java
@@ -6,12 +6,26 @@ import java.util.stream.IntStream;
 
 public class Game {
 
-    int gameSize;
-    String boardFill;
+    UserAssist playerAssist = new UserAssist();
 
-    public Game(int gameSize, String boardFill) {
+    final int gameSize;
+    final String boardFill;
+    final String shipMark;
+    final String shipHitMark;
+    final String hitMsg;
+    final String shipMissMark;
+    final String missMsg;
+
+    public Game(int gameSize, String boardFill,
+                String shipMark,
+                String shipHitMark, String hitMsg, String shipMissMark, String missMsg) {
         this.gameSize = gameSize;
         this.boardFill = boardFill;
+        this.shipMark = shipMark;
+        this.shipHitMark = shipHitMark;
+        this.hitMsg = hitMsg;
+        this.shipMissMark = shipMissMark;
+        this.missMsg = missMsg;
     }
 
     String[] colNames;
@@ -20,24 +34,13 @@ public class Game {
     String[][] gameProgress;
 
     boolean startUp = true;
+    boolean gameStart = true;
+
     final String space = " ";
 
     int numOfShips = Ships.values().length;
     int[][][] allShipsCoords = new int[numOfShips][][];
 
-    public void setUpBoard() {
-
-        this.gameProgress = new String[this.gameSize][this.gameSize];
-        this.colNames = IntStream.rangeClosed(1, this.gameSize)
-                .mapToObj(String::valueOf)
-                .toArray(String[]::new);
-        this.rowNames = IntStream.range('A', 'A' + this.gameSize)
-                .mapToObj(num -> (char) num)
-                .toArray(Character[]::new);
-        for (String[] row : this.gameProgress) {
-            Arrays.setAll(row, i -> this.boardFill);
-        }
-    }
 
     public void printBoard() {
 
@@ -49,17 +52,30 @@ public class Game {
             System.out.print(space.repeat(2));
         }
 
-        System.out.println(String.join(space, this.colNames));
-        for (int i = 0; i < this.gameProgress.length; i++) {
+        System.out.println(String.join(space, colNames));
+        for (int i = 0; i < gameProgress.length; i++) {
             System.out.print(rowNames[i] + space);
-            System.out.println(String.join(space, this.gameProgress[i]));
+            System.out.println(String.join(space, gameProgress[i]));
         }
+    }
+
+    public void setUpBoard() {
+
+        this.gameProgress = new String[this.gameSize][this.gameSize];
+        this.colNames = IntStream.rangeClosed(1, this.gameSize)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+        this.rowNames = IntStream.range('A', 'A' + this.gameSize)
+                .mapToObj(num -> (char) num)
+                .toArray(Character[]::new);
+        for (String[] row : gameProgress) {
+            Arrays.setAll(row, i -> this.boardFill);
+        }
+        printBoard();
     }
 
 
     public void placeShip(Ships ship) {
-
-        UserAssist playerAssist = new UserAssist();
 
         int[][] shipCoords = playerAssist.buildShip(allShipsCoords, ship);
 
@@ -67,14 +83,43 @@ public class Game {
         for (int[] xyPair : shipCoords) {
             int row = xyPair[0];
             int col = xyPair[1];
-            this.gameProgress[row][col] = "O";
+            gameProgress[row][col] = this.shipMark;
         }
+        printBoard();
 
         // add to all ships 3d array [[[x, y],[x, y],[x, y]], ..., ..., ... ...]
         allShipsCoords[ship.ordinal()] = shipCoords;
     }
+
+    public void placeShot() {
+        if (gameStart) {
+            System.out.printf("%nThe game starts!%n");
+            gameStart = false;
+            printBoard();
+        }
+
+        String[] shotOutcomes = new String[] { this.missMsg, this.hitMsg };
+        String[] shotMarks = new String[] { this.shipMissMark, this.shipHitMark };
+
+        int[] shotCoords = playerAssist.takeShot(gameSize);
+        int row = shotCoords[0];
+        int col = shotCoords[1];
+
+        int shotResult = 1 & Boolean.hashCode( gameProgress[row][col] == this.shipMark) >> 1;
+
+        // draw on board
+        gameProgress[row][col] = shotMarks[shotResult];
+        printBoard();
+
+        // update all ships 3d array [[[-1, -1],[x, y],[x, y]], ..., ..., ... ...]
+        for (int i = 0; i < allShipsCoords.length; i++) {
+            for (int j = 0; j < allShipsCoords[i].length; j++) {
+                if (Arrays.equals(allShipsCoords[i][j], shotCoords)) {
+                    allShipsCoords[i][j] = new int[] {-1, -1};
+                }
+            }
+        }
+
+        System.out.printf("%nYou %s!%n",  shotOutcomes[shotResult]);
+    }
 }
-
-
-
-
