ea3aa1f4b3be6e27e22a3ffe4a4f5a0fa1b89d85
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-09-26 16:07:21 +1100

    Stage 5

--- Game.java
+++ Game.java
@@ -3,12 +3,15 @@ package battleship;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 
 public class Game {
 
     UserAssist playerAssist = new UserAssist();
+    User player1 = new User();
+    User player2 = new User();
 
     final int gameSize;
     final String boardFill;
@@ -35,131 +38,141 @@ public class Game {
         this.gameOverMsg = gameOverMsg;
     }
 
-    static boolean gameOver = false;
-
     String[] colNames;
     Character[] rowNames;
-
-    String[][] gameProgress;
-
-    boolean startUp = true;
-    boolean gameStart = true;
-
     final String space = " ";
 
     int numOfShips = Ships.values().length;
-    int[][][] allShipsCoords = new int[numOfShips][][];
+    static boolean gameOver = false;
 
-    int[] shipsHealth = new int[Ships.values().length];
-    ArrayList<String> shipsSank = new ArrayList<>();
+    //---------------------------------METHODS---------------------------------
 
+    User player = player1;
 
-    public void printBoard(String mode) {
+    public void switchPlayer(User player) {
+        this.player = player == player1 ? player2 : player1;
+    }
 
-        if (startUp) {
-            System.out.print(space.repeat(2));
-            startUp = false;
-        } else {
-            System.out.println();
-            System.out.print(space.repeat(2));
-        }
+    public static void enterToCont() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("%nPress Enter and pass the move to another player");
+        scanner.nextLine();
+    }
 
+    public void printBoard(String mode, User player) {
+
+        System.out.println();
         System.out.println(String.join(space, colNames));
-        for (int i = 0; i < gameProgress.length; i++) {
+        for (int i = 0; i < player.field.length; i++) {
             System.out.print(rowNames[i] + space);
             if (mode.equals("showShips")) {
-                System.out.println(String.join(space, gameProgress[i]));
+                System.out.println(String.join(space, player.field[i]));
             } else if (mode.equals("hideShips")) {
-                ArrayList<String> row = new ArrayList<>(Arrays.asList(gameProgress[i]));
+                ArrayList<String> row = new ArrayList<>(Arrays.asList(player.field[i]));
                 Collections.replaceAll(row, this.shipMark, this.boardFill);
                 System.out.println(String.join(space, row));
             }
         }
     }
 
-    public void setUpBoard() {
-
-        this.gameProgress = new String[this.gameSize][this.gameSize];
+    public void setUp(String name) {
+        player.setName(name);
+        player.field = new String[gameSize][gameSize];
         this.colNames = IntStream.rangeClosed(1, this.gameSize)
                 .mapToObj(String::valueOf)
                 .toArray(String[]::new);
         this.rowNames = IntStream.range('A', 'A' + this.gameSize)
                 .mapToObj(num -> (char) num)
                 .toArray(Character[]::new);
-        for (String[] row : gameProgress) {
+        for (String[] row : player.field) {
             Arrays.fill(row, this.boardFill);
         }
-        printBoard("showShips");
+        System.out.printf(
+                "%n%s, place your ships on the game field\n", player.playerName);
+        printBoard("showShips", player);
     }
-
 
     public void placeShip(Ships ship) {
 
-        int[][] shipCoords = playerAssist.buildShip(allShipsCoords, ship);
+        String shipType = ship.getType();
+        int shipSize = ship.getSize();
+
+        System.out.printf(
+                "%nEnter the coordinates of the %s (%d cells):%n%n", shipType, shipSize);
+        int[][] shipCoords = playerAssist.buildShip(player.myShipsCoords, shipType, shipSize);
 
         // draw on board
         for (int[] xyPair : shipCoords) {
             int row = xyPair[0], col = xyPair[1];
-            gameProgress[row][col] = this.shipMark;
+            player.field[row][col] = this.shipMark;
         }
-        printBoard("showShips");
+        printBoard("showShips", player);
 
         // add ship coords to all ships 3d array [[[x, y],[x, y],[x, y]], ..., ..., ... ...]
-        allShipsCoords[ship.ordinal()] = shipCoords;
+        player.myShipsCoords[ship.ordinal()] = shipCoords;
 
         // add ship health(size) to all ships 1d array [5, 4, 3, ...]
-        shipsHealth[ship.ordinal()] = Ships.values()[ship.ordinal()].getSize();
+        player.myShipsHealth[ship.ordinal()] = Ships.values()[ship.ordinal()].getSize();
+
+        if (ship.ordinal() == Ships.values().length - 1) {
+            switchPlayer(player);
+            enterToCont();
+        }
 
     }
 
     public void placeShot() {
-        if (gameStart) {
-            System.out.printf("%nThe game starts!%n");
-            printBoard("hideShips");
-            System.out.printf("%nTake a shot!%n%n");
-            gameStart = false;
-        }
 
-        String[] shotOutcomes = new String[] { this.missMsg, this.hitMsg, this.sankMsg };
+        switchPlayer(player); // switch to opponent
+        printBoard("hideShips", player);
+        System.out.println("-".repeat(gameSize * 2));
+
+        switchPlayer(player); // switch to self
+        printBoard("showShips", player);
+        System.out.printf("%n%s, it's your turn:%n%n", player.playerName);
+
+
+        String[] shotOutcomes = new String[] { this.missMsg, this.hitMsg, this.sankMsg, this.gameOverMsg };
         String[] shotMarks = new String[] { this.shipMissMark, this.shipHitMark };
 
         int[] shotCoords = playerAssist.takeShot(gameSize);
         int row = shotCoords[0], col = shotCoords[1];
 
+        switchPlayer(player); // switch to opponent
         int shotResult = 1 & Boolean.hashCode(Arrays.asList(this.shipMark, this.shipHitMark)
-                                                    .contains(gameProgress[row][col])) >> 1;
+                                                    .contains(player.field[row][col])) >> 1;
 
-        // draw on board
-        gameProgress[row][col] = shotMarks[shotResult];
-        printBoard("hideShips");
+        // update game progress @opponent
+        player.field[row][col] = shotMarks[shotResult];
 
-        // update ships health
-        for (int i = 0; i < allShipsCoords.length; i++) {
-            for (int j = 0; j < allShipsCoords[i].length; j++) {
-                if (Arrays.equals(allShipsCoords[i][j], shotCoords)) {
-                    allShipsCoords[i][j] = new int[] {-1, -1};
-                    shipsHealth[i]--;
+        // update ships health @opponent
+        for (int i = 0; i < player.myShipsCoords.length; i++) {
+            for (int j = 0; j < player.myShipsCoords[i].length; j++) {
+                if (Arrays.equals(player.myShipsCoords[i][j], shotCoords)) {
+                    player.myShipsCoords[i][j] = new int[] {-1, -1};
+                    player.myShipsHealth[i]--;
                     break;
                 }
             }
         }
 
-        // update sank ships list
-        for (int i = 0; i < shipsHealth.length; i++) {
-            if (shipsHealth[i] == 0) {
-                shipsSank.add(Ships.values()[i].getType());
-                shipsHealth[i]--;
+        // update sank ships list @opponent
+        for (int i = 0; i < player.myShipsHealth.length; i++) {
+            if (player.myShipsHealth[i] == 0 && player.myShipsSank.size() != Ships.values().length + 1) {
+                player.myShipsSank.add(Ships.values()[i].getType());
+                player.myShipsHealth[i]--;
                 shotResult++;
                 break;
             }
         }
 
         // shot result msg display
-        gameOver = shipsSank.size() == Ships.values().length;
+        gameOver = player.myShipsSank.size() == numOfShips + 1; // sank ships list starts from 1
+        if (gameOver) {  shotResult++; }
+        System.out.printf("%n%s" + "%n".repeat(shotResult == 3 ? 1 : 0),
+                String.format(shotOutcomes[shotResult], player.myShipsSank.get(player.myShipsSank.size() - 1)));
         if (!gameOver) {
-            System.out.printf("%n%s%n%n", shotOutcomes[shotResult]);
-        } else {
-            System.out.printf("%n%s%n", this.gameOverMsg);
+            enterToCont();
         }
     }
 }
