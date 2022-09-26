d2cc8197554f541c7a336aaeceef58770a3bd64c
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-09-26 16:04:09 +1100

    Stage 5

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package battleship;
 
-import java.util.Arrays;
 
 public class Main {
 
@@ -10,21 +9,22 @@ public class Main {
         final String boardFill = "~";
         final String shipMark = "O";
         final String shipHitMark = "X";
-        final String hitMsg = "You hit a ship! Try again:";
+        final String hitMsg = "You hit a ship!";
         final String shipMissMark = "M";
-        final String missMsg = "You missed. Try again:";
-        final String sankMsg = "You sank a ship! Specify a new target:";
-        final String gameOverMsg = "You sank the last ship. You won. Congratulations!";
+        final String missMsg = "You missed.";
+        final String sankMsg = "You sank a %s ship! Specify a new target:";
+        final String gameOverMsg = "You sank the last ship (%s). You won. Congratulations!";
+        final String[] players = {"Player 1", "Player 2"};
 
         Game myGame = new Game(gameSize, boardFill, shipMark,
                 shipHitMark, hitMsg, shipMissMark, missMsg, sankMsg, gameOverMsg);
 
-        // set up initial game board
-        myGame.setUpBoard();
-
-        // place all ships on board
-        for (Ships ship : Ships.values()) {
-            myGame.placeShip(ship);
+        // set up players and their board
+        for (String playerName : players) {
+            myGame.setUp(playerName);
+            for (Ships ship : Ships.values()) {
+                myGame.placeShip(ship);
+            }
         }
 
         // taking shots till all ships in sank list
