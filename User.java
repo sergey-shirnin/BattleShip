c050eb79aab3a89c75d247cfc151ef646240ac57
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-09-26 15:54:06 +1100

    Stage 4

--- User.java
+++ User.java
@@ -1,24 +1,18 @@
 package battleship;
 
-
 import java.util.Scanner;
+
 
 public class User {
 
     final String space = " ";
-
     Scanner scanner = new Scanner(System.in);
 
-    public String[] initiateShip(String shipType, int shipSize) {
-
-        System.out.printf(
-                "%nEnter the coordinates of the %s (%d cells):%n%n", shipType, shipSize);
+    public String[] initiateShip() {
         return scanner.nextLine().split(space);
     }
 
     public String takeShot() {
-        System.out.printf(
-                "%nTake a shot!%n%n");
         return scanner.nextLine();
     }
 
